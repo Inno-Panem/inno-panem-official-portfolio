bce463bb4fcbdbea7198f13814f830e0208a36c6
Inno Panem <panemj1@mcmaster.ca>
2022-09-16 23:10:10 -0400

    Active Class Implementation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,21 +1,36 @@
-import "./intro.scss"
+import "./intro.scss";
 import { ArrowDropDownCircleTwoTone } from "@material-ui/icons";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,  // waits once all letters are shown
+      backSpeed: 60,
+      showCursor: true,
+      strings: ["Computer", "Embedded Systems", "Software"],
+    });
+  }, [])
+
   return (
     <div className = "intro" id = "intro">
       <div className="left">
         <div className="imgContainer">
-            <img src="assets/IMG_5249.png" alt=""/>
+            <img src="assets/IMG_5249.png" alt="Inno Panem"/>
         </div>
       </div>
       <div className="right">
         <div className = "wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Inno Panem</h1>
-          <h3>Computer Engineering Student<span></span></h3>
+          <h3>Aspiring <span ref={textRef}></span> Engineer</h3>
         </div>
         <a href="#portfolio">
           <ArrowDropDownCircleTwoTone/>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,75 @@
-import "./portfolio.scss"
+import "./portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
+import { useEffect, useState } from "react";
 
 export default function Portfolio() {
+
+  const [selected, setSelected] = useState("featured");
+
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
+
   return (
     <div className = "portfolio" id="portfolio">
-    
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item)=>(
+          <PortfolioList 
+            title={item.title} 
+            active={selected === item.id} 
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      {/* Represents one section of the portfolio */}
+      <div className = "container"> 
+        <div className="item">
+          <img src="assets/Last_Night_Was_A_Movie.png" alt="album-cover"></img>
+          <h3>Last Night Was A Movie</h3>
+        </div>
+
+        <div className="item">
+          <img src="assets/12amDeluxe.png" alt="album-cover"></img>
+          <h3>12AM On Jane & Wilson Deluxe</h3>
+        </div>
+
+        <div className="item">
+          <img src="assets/navy_album.png" alt="album-cover"></img>
+          <h3>Navy</h3>
+        </div>
+
+        <div className="item">
+          <img src="assets/triptobatangasrework.png" alt="album-cover"></img>
+          <h3>Trip to Batangas</h3>
+        </div>
+
+        <div className="item">
+          <img src="assets/Vibe.png" alt="album-cover"></img>
+          <h3>No Dawgs Allowed</h3>
+        </div>
+      </div>
+
     </div>
   )
 }
