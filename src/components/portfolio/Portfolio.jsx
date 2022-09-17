fe89158413ca432828ca04d8a1a58264e5f2e881
Inno Panem <panemj1@mcmaster.ca>
2022-09-16 23:56:04 -0400

    Added data.js and using data with useEffect hook

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,16 @@
 import "./portfolio.scss";
 import PortfolioList from "../portfolioList/PortfolioList";
 import { useEffect, useState } from "react";
+import { featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contentPortfolio} from "../../data";
 
 export default function Portfolio() {
 
   const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
 
   const list = [
     {
@@ -29,6 +35,32 @@ export default function Portfolio() {
     },
   ];
 
+  useEffect(()=>{
+
+    switch(selected){
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+        case "web":
+          setData(webPortfolio);
+          break;
+        case "mobile":
+          setData(mobilePortfolio);
+          break;
+        case "design":
+          setData(designPortfolio);
+          break;
+        case "content":
+          setData(contentPortfolio);
+          break;
+        default:
+          setData(featuredPortfolio);
+          
+  
+    }
+
+  },[selected])
+
   return (
     <div className = "portfolio" id="portfolio">
       <h1>Portfolio</h1>
@@ -44,32 +76,18 @@ export default function Portfolio() {
       </ul>
       {/* Represents one section of the portfolio */}
       <div className = "container"> 
-        <div className="item">
-          <img src="assets/Last_Night_Was_A_Movie.png" alt="album-cover"></img>
-          <h3>Last Night Was A Movie</h3>
-        </div>
-
-        <div className="item">
-          <img src="assets/12amDeluxe.png" alt="album-cover"></img>
-          <h3>12AM On Jane & Wilson Deluxe</h3>
-        </div>
-
-        <div className="item">
-          <img src="assets/navy_album.png" alt="album-cover"></img>
-          <h3>Navy</h3>
-        </div>
-
-        <div className="item">
-          <img src="assets/triptobatangasrework.png" alt="album-cover"></img>
-          <h3>Trip to Batangas</h3>
-        </div>
-
-        <div className="item">
-          <img src="assets/Vibe.png" alt="album-cover"></img>
-          <h3>No Dawgs Allowed</h3>
-        </div>
+          {data.map(d=>(
+            <div className="item">
+              <img 
+              // src = {d.img}
+              src="assets/Last_Night_Was_A_Movie.png" 
+              alt="album-cover"
+              />
+              {/* {d.title} */}
+              <h3>Last Night Was A Movie</h3> 
+            </div>
+          ))}
       </div>
-
     </div>
   )
 }
